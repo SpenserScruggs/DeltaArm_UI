81d777b6ca2011b0e8e09c84691eacbc3ee02c80
Spenser Scruggs <scru7556@vandals.uidaho.edu>
2022-04-10 19:04:26 -0700

    Adds a record feature

--- DeltaArm_UI/MainWindow.xaml.cs
+++ DeltaArm_UI/MainWindow.xaml.cs
@@ -23,23 +23,36 @@ namespace DeltaArm_UI
     public partial class MainWindow : Window
     {
         private SerialPort port = new SerialPort();
+
         public bool running = false;
+
+        public bool recording = false;
+        static public int save_length = 1000;
+        public string[] recordSaved = new string[save_length];
+
+        public int color = 0;
         
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
         private void inkCanvas1_Gesture(object sender, InkCanvasGestureEventArgs e)
         {
         }
+
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             this.inkCanvas1.Strokes.Clear();
         }
+
         private void button2_Click(object sender, RoutedEventArgs e)
         {
+            port.Close();
             this.Close();
         }
+
         private void OnKeyDownHandler(object sender, System.Windows.Input.KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
@@ -47,11 +60,12 @@ namespace DeltaArm_UI
                 this.inkCanvas1.Strokes.Clear();
             }
         }
+
         private void Button4_Click(object sender, RoutedEventArgs e)
         {
-            port.Close();
             running = false;
         }
+
         private void Button3_Click(object sender, RoutedEventArgs e)
         {
             port = new SerialPort();
@@ -67,6 +81,34 @@ namespace DeltaArm_UI
             {
                 System.Windows.MessageBox.Show(e1.Message);
             }
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            if (recording)
+            {
+                recording = false;
+            }
+            else if (!recording)
+            {
+                
+                recordSaved = new string[save_length];
+                recording = true;
+            }
+            if (color % 2 == 0)
+            {
+                button5.Background = button5.Background == Brushes.Red ? (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFDDDDDD")) : Brushes.Green;
+            }
+            else
+            {
+                button5.Background = button5.Background == Brushes.Red ? (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFDDDDDD")) : Brushes.LightGray;
+            }
+            color++;
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            PlayRecording();
         }
         
         private string ReSize(int x)
@@ -92,28 +134,52 @@ namespace DeltaArm_UI
                 bool mouseIsDown = System.Windows.Input.Mouse.LeftButton == MouseButtonState.Pressed;
 
                 int x = Convert.ToInt32(Mouse.GetPosition(inkCanvas1).X);
-                int y = Convert.ToInt32(Mouse.GetPosition(inkCanvas1).Y);
-                int z = 100;
+                int y = 600 - Convert.ToInt32(Mouse.GetPosition(inkCanvas1).Y);
 
+                int z = 200;
                 string x1, y1, z1;
 
                 if ((x >= 0) && (x <= 800) && (y >= 0) && (y <= 600))
                 {
                     if (mouseIsDown)
                     {
-                        z = 150;
+                        z = 218;
                     }
                     x1 = ReSize(x);
                     y1 = ReSize(y);
                     z1 = ReSize(z);
+
                     Trace.WriteLine(x1 + "," + y1 + "," + z1);
-                    
-                    if (port.IsOpen)
+                    if (port.IsOpen && !recording)
                     {
                         port.WriteLine(x1 + y1 + z1);
                     }
+                    else if (recording)
+                    {
+                        string newPos = x1 + y1 + z1;
+                        List<string> list = new List<string>(recordSaved.ToList());
+                        list.Add(newPos);
+                        recordSaved = list.ToArray();
+                    }
                 }
-                await Task.Delay(10);
+                await Task.Delay(15);
+            }
+        }
+
+        public async void PlayRecording()
+        {
+            for (int i = 0; i < recordSaved.Length; i++)
+            {
+                if (port.IsOpen && (recordSaved[i] != null))
+                {
+                    port.WriteLine(recordSaved[i]);
+                    await Task.Delay(15);
+                }
+                if (recordSaved[i] != null)
+                {
+                    Trace.WriteLine(recordSaved[i]);
+                    await Task.Delay(15);
+                }
             }
         }
     }
